7d6719d2ee3b0e95304024d39a80063ade4642b9
Lalita Mittal <MittalL@delawareconsulting.com>
2022-06-09 14:46:06 +0200

    first
    first commit

---
+++ TeamsApp-WebCulture/Model/SearchModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeamsApp_WebCulture
+{
+    public class SearchResult
+    {
+        public SearchModels d { get; set; }
+    }
+    public class SearchModels
+    {
+        public SearchModel[] results { get; set; }
+    }
+
+    public class SearchModel
+    {
+       
+        public string ID { get; set; }
+        public string EntityType { get; set; }
+       
+        public string DivisionDescription { get; set; }
+       
+        public string Description { get; set; }
+       
+    }
+}
